2e7fc4ecccd5913b0266bb97d0e8955ca1b9a466
Leandro Meyer <leandro.meyer@gmail.com>
2022-10-01 23:23:41 -0300

    style changes and insert carousel

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,25 @@
 console.log('hello')
+
+const $simpleCarousel = document.querySelector('.js-carousel--simple');
+
+new Glider($simpleCarousel, {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: true,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+  ]
+});
