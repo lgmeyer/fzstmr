f9585c70aaf8e8f7be6463626d0c8b9cb32ab307
Leandro Meyer <leandro.meyer@gmail.com>
2022-10-03 08:42:48 -0300

    change carousel images settings

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-console.log('hello')
-
 const $simpleCarousel = document.querySelector('.js-carousel--simple');
 
 new Glider($simpleCarousel, {
@@ -23,3 +21,31 @@ new Glider($simpleCarousel, {
     },
   ]
 });
+
+// function sliderAuto(slider, miliseconds) {
+//   const slidesCount = slider.track.childElementCount;
+//   let slideTimeout = null;
+//   let nextIndex = 1;
+ 
+//   function slide () {
+//     slideTimeout = setTimeout(
+//       function () {
+//         if (nextIndex >= slidesCount ) {
+//           nextIndex = 0;
+//           nextIndex = nextIndex;
+//         }
+//         slider.scrollItem(nextIndex++);
+//       },
+//       miliseconds
+//     );
+//   }
+ 
+//   slider.ele.addEventListener('glider-animated', function() {
+//     window.clearInterval(slideTimeout);
+//     slide();
+//   });
+ 
+//   slide();
+//  }
+ 
+//  sliderAuto(glider, 3000)
